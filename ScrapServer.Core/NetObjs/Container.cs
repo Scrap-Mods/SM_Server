0977aa5b9d69fea6ca3658eb73749fd86ae91eb6
TechnologicNick <technologicnick@gmail.com>
2024-08-18 22:53:15 +0200

    Add ContainerService with CollectToSlot
    Not tested nor integrated yet

---
+++ ScrapServer.Core/NetObjs/Container.cs
@@ -0,0 +1,65 @@
+﻿using System.Drawing;
+
+namespace ScrapServer.Core.NetObjs;
+
+public class Container
+{
+    public readonly uint Id;
+    public readonly ushort MaximumStackSize;
+    public readonly ItemStack[] Items;
+
+    public record ItemStack(Guid Uuid, uint InstanceId, ushort Quantity)
+    {
+        public const uint NoInstanceId = uint.MaxValue;
+
+        public static ItemStack Empty => new(Uuid: Guid.Empty, InstanceId: NoInstanceId, Quantity: 0);
+
+        public bool IsEmpty => Uuid == Guid.Empty || Quantity == 0;
+
+        public bool IsStackableWith(ItemStack other)
+        {
+            if (other == null) return false;
+
+            if (this.IsEmpty || other.IsEmpty) return true;
+
+            return this.Uuid == other.Uuid && this.InstanceId == other.InstanceId;
+        }
+
+        public static ItemStack Combine(ItemStack a, ItemStack b)
+        {
+            if (!a.IsStackableWith(b))
+            {
+                throw new InvalidOperationException("Cannot add two ItemStacks that are not stackable");
+            }
+
+            var quantity = a.Quantity + b.Quantity;
+
+            if (quantity > ushort.MaxValue)
+            {
+                throw new InvalidOperationException("Cannot add two ItemStacks that would overflow the quantity");
+            }
+
+            var baseItemStack = !a.IsEmpty ? a : b;
+
+            return baseItemStack with { Quantity = (ushort)quantity };
+        }
+    }
+
+    public Container(uint id, ushort size, ushort maximumStackSize = ushort.MaxValue)
+    {
+        this.Id = id;
+        this.MaximumStackSize = maximumStackSize;
+        this.Items = Enumerable.Repeat(ItemStack.Empty, size).ToArray();
+    }
+
+    public Container Clone()
+    {
+        var clone = new Container(this.Id, (ushort)Items.Length);
+        for (int i = 0; i < this.Items.Length; i++)
+        {
+            clone.Items[i] = this.Items[i];
+        }
+
+        return clone;
+    }
+}
---
+++ ScrapServer.Core/Services/ContainerService.cs
@@ -0,0 +1,113 @@
+﻿using ScrapServer.Core.NetObjs;
+using static ScrapServer.Core.NetObjs.Container;
+
+namespace ScrapServer.Core;
+
+public class ContainerService
+{
+    public Dictionary<uint, Container> Containers = [];
+
+    private volatile bool IsInTransaction = false;
+
+    public class Transaction(ContainerService containerService) : IDisposable
+    {
+        private readonly Dictionary<uint, Container> modified = [];
+
+        /// <summary>
+        /// Collects items into a specific slot of a container.
+        /// </summary>
+        /// <param name="container">The container to collect the items into</param>
+        /// <param name="itemStack">The item stack, including quantity, to collect</param>
+        /// <param name="mustCollectAll">
+        ///     If true, only collect items if the full item stack fits in the remaining space of the slot.
+        ///     If false, collect as many items that fit into the remaining space, and do so without overflowing into other slots.
+        /// </param>
+        /// <returns>A tuple containing the number of items collected and the result of the operation</returns>
+        public (ushort Collected, OperationResult Result) CollectToSlot(Container container, ItemStack itemStack, ushort slot, bool mustCollectAll = true)
+        {
+            var containerCopyOnWrite = modified[container.Id] ?? container.Clone();
+
+            if (slot < 0 || slot > containerCopyOnWrite.Items.Length)
+            {
+                throw new SlotIndexOutOfRangeException($"Slot {slot} is out of range [0, {containerCopyOnWrite.Items.Length})");
+            }
+
+            var currentItemStackInSlot = containerCopyOnWrite.Items[slot];
+
+            if (!currentItemStackInSlot.IsStackableWith(itemStack))
+            {
+                return (0, OperationResult.NotStackable);
+            }
+
+            int max = containerService.GetMaximumStackSize(container, itemStack.Uuid);
+            int remainingSpace = max - currentItemStackInSlot.Quantity;
+
+            int quantityToCollect = Math.Min(remainingSpace, itemStack.Quantity);
+            if (quantityToCollect <= 0)
+            {
+                return (0, OperationResult.NotEnoughSpace);
+            }
+
+            if (mustCollectAll && quantityToCollect < itemStack.Quantity)
+            {
+                return (0, OperationResult.NotEnoughSpaceForAll);
+            }
+
+            containerCopyOnWrite.Items[slot] = ItemStack.Combine(itemStack, currentItemStackInSlot);
+
+            modified[container.Id] = containerCopyOnWrite;
+
+            return ((ushort)quantityToCollect, OperationResult.Success);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposing)
+            {
+                return;
+            }
+
+            if (!containerService.IsInTransaction)
+            {
+                throw new InvalidOperationException("Transaction was not committed or rolled back");
+            }
+        }
+
+        public class SlotIndexOutOfRangeException(string message) : Exception(message)
+        {
+        }
+
+        public enum OperationResult
+        {
+            Success,
+            NotStackable,
+            NotEnoughSpace,
+            NotEnoughSpaceForAll,
+        }
+    }
+
+    public Transaction BeginTransaction()
+    {
+        if (this.IsInTransaction)
+        {
+            throw new InvalidOperationException("Cannot start a transaction while one is already in progress");
+        }
+
+        this.IsInTransaction = true;
+
+        return new Transaction(this);
+    }
+
+    public ushort GetMaximumStackSize(Container container, Guid uuid)
+    {
+        // TODO: Return the minimum of the maximum stack size of the item and the container
+
+        return container.MaximumStackSize;
+    }
+}
